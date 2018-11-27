30395dc6edfa8d2b0308d2a541465b0b45c5da11
Mikian Musser <Bobar312@gmail.com>
2018-11-27 11:45:35 -0800

    Updated Data Structure

--- notes/mikian/dataStructure.js
+++ notes/mikian/dataStructure.js
@@ -1,41 +1,33 @@
 let user = {
   "_id": "UNIQUEID",
   "name": "Mikian",
-  "numreflections": 3,
-  "numAnswers" : 6,
-  "settings": {
-    "x" : X,
-    "y" : Y
-   }
+}
+
+let question = {
+  "_id": "UNIQUEID",
+  "text": "Highlight",
 }
 
 let reflection = {
   "_id": "UNIQUEID",
   "user_id": "UNIQUEUSERID",
-  "numAnswers" : 2,
   "time": {
     "date": "2016-05-18T16:00:00Z",
-    "offset": 420
+    "offset": 120
   },
   "answers": [
     {
-      "question": "Something good:",
-      "question_id": 0,
+      "question_id": "UNIQUEQUESTIONID",
       "answertext": "I learned alot."
     },
     {
-      "question": "Something Bad:",
-      "question_id": 1,
+      "question_id": "UNIQUEQUESTIONID",
       "answertext": "It took a long time."
     }
   ]
 }
 
-let question = {
-  "_id": "UNIQUEID",
-  "text": "Something good:",
-  "numberOfTimesAnswered" : 2
-}
+
 
 
 
